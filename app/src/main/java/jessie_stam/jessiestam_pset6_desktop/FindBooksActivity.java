2a1a2aeaac4dc204a7ecd578019c392b3831d67f
Jessie <jessiestam@msn.com>
2016-10-20 23:06:24 +0200

    Initial commit

---
+++ app/src/main/java/jessie_stam/jessiestam_pset6_desktop/FindBooksActivity.java
@@ -0,0 +1,20 @@
+package jessie_stam.jessiestam_pset6_desktop;
+
+import android.os.Bundle;
+
+/**
+ * Created by Jessie on 15-10-2016.
+ */
+
+public class FindBooksActivity extends EmailPasswordActivity {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
---
+++ app/src/main/java/jessie_stam/jessiestam_pset6_desktop/LogInSignUpActivity.java
@@ -0,0 +1,8 @@
+package jessie_stam.jessiestam_pset6_desktop;
+
+/**
+ * Created by Jessie on 14/10/2016.
+ */
+
+public class LogInSignUpActivity {
+}
